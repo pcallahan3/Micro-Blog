8bb1708c6b83ccea58447d54e398fd6e44cb17f5
Patrick Callahan <railsudemycourse@gmail.com>
2016-12-02 17:20:15 +0000

    Add edit, update, show, and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,22 @@
 #Implementing a new articles_controller
 class ArticlesController < ApplicationController
   
+  def index
+    @articles = Article.all
+  end
+  
   #new method to create a new article
   def new
     @article = Article.new
     
   end
   
+  #edit method that finds the article id to edit
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+   #show method that finds the id of article to show
    def show
     @article = Article.find(params[:id])
   end
@@ -25,11 +35,21 @@ class ArticlesController < ApplicationController
     end
   end
   
+  #update action to update article, if succesful render message, else rended edit template again
+  def update
+    @article = Article.find(params[:id])  
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  
+  
  
   
-  
-  
-  #
+  #whitelisted parameters that must be included in CRUDing a article
   private def article_params
     params.require(:article).permit(:title, :description)
   end
